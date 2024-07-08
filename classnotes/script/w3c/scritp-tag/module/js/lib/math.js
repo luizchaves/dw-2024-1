af098cd222bd7e8e11a868e2fa0b205c31a91937
Luiz Carlos <lucachaves@gmail.com>
2024-07-08 12:46:16 -0300

    fix: script-tag

--- classnotes/script/w3c/scritp-tag/module/js/lib/math.js
+++ classnotes/script/w3c/scritp-tag/module/js/lib/math.js
@@ -1,3 +1,3 @@
-function sum(a, b) {
+export function sum(a, b) {
   return a + b;
 }
--- classnotes/script/w3c/scritp-tag/module/js/main.js
+++ classnotes/script/w3c/scritp-tag/module/js/main.js
@@ -1,6 +1,3 @@
 import { sum } from './lib/math.js';
-// import { sqrt } from 'mathjs';
-import { sqrt } from './node_modules/mathjs/lib/browser/math.js';
 
 console.log(sum(1, 1));
-console.log(math.sqrt(4));
