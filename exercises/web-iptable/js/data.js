c62c9fa4456f53b3d2d27a0cba0594dc83c008f1
Luiz Carlos <lucachaves@gmail.com>
2024-07-15 07:53:34 -0300

    exercises: web-iptable

---
+++ exercises/web-iptable/js/data.js
@@ -0,0 +1,27 @@
+export const hosts = [
+  {
+    address: '192.168.0.1',
+    mask: '255.255.255.0',
+    version: 'v4',
+  },
+  {
+    address: '192.168.0.2',
+    mask: '255.255.255.0',
+    version: 'v4',
+  },
+  {
+    address: '192.168.0.3',
+    mask: '255.255.255.0',
+    version: 'v4',
+  },
+  {
+    address: '192.168.0.4',
+    mask: '255.255.255.0',
+    version: 'v4',
+  },
+  {
+    address: '192.168.0.5',
+    mask: '255.255.255.0',
+    version: 'v4',
+  },
+];
